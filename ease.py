9499e26e89018676da02c24d0cf3abad17664f1d
cympfh <cympfh@gmail.com>
2022-06-06 02:57:10 +0900

    random experiments

--- ease.py
+++ ease.py
@@ -1,4 +1,5 @@
 import numpy
+import tqdm
 
 
 class EaseModel:
@@ -60,18 +61,44 @@ class EaseApprox(EaseModel):
         gd = numpy.diag(G)
         B = (G > 0) * 1.0
         B[diags] = 0.0
-        lr = 0.1
-        for _ in range(100):
+        lr = 1.0
+        for _ in tqdm.tqdm(range(1000)):
             Z = -G + (gd * B.T).T
             Z += lambda_
+            Z = Z.clip(min=-2.0, max=2.0)
             B -= Z * lr
-            B -= B * lr * lambda_
             B[diags] = 0.0
-            B2 = B * 0.9
-            if self.loss(X, B2, lambda_) < self.loss(X, B, lambda_):
-                B = B2
+            lr = max(0.001, lr * 0.99)
+        while self.loss(X, B * 0.9, lambda_) < self.loss(X, B, lambda_):
+            B *= 0.9
         return B.astype(numpy.float32)
 
+
+def test(name, modelClass, X, lambda_, small=True):
+    print(f"# {name} -- lambda={lambda_}")
+    print("X=")
+    if small:
+        print(X)
+    else:
+        print(X[:5, :5])
+    model = modelClass()
+    loss = model.fit(X, lambda_)
+    print(f"{loss=}")
+    print("model.B=")
+    print(model.B if small else model.B[:5, :5])
+    print("model.predict=")
+    print(model.predict() if small else model.predict()[:5, :5])
+    print()
+
+
+X_trivial = numpy.array(
+    [
+        [1, 1, 0],
+    ],
+    dtype="f",
+)
+test("EaseExact with X_trivial", EaseExact, X_trivial, 0.0001)
+test("EaseApprox with X_trivial", EaseApprox, X_trivial, 0.0001)
 
 X = numpy.array(
     [
@@ -81,31 +108,19 @@ X = numpy.array(
     ],
     dtype="f",
 )
-
-X_trivial = numpy.array(
-    [
-        [1, 1, 0],
-    ],
-    dtype="f",
-)
-
-
-def test(name, modelClass, X, lambda_):
-    print(f"# {name} -- lambda={lambda_}")
-    print("X=")
-    print(X)
-    model = modelClass()
-    loss = model.fit(X, lambda_)
-    print(f"{loss=}")
-    print("model.B=")
-    print(model.B)
-    print("model.predict=")
-    print(model.predict())
-    print()
-
-
-test("EaseExact with X_trivial", EaseExact, X_trivial, 0.0001)
-test("EaseApprox with X_trivial", EaseApprox, X_trivial, 0.0001)
-
 test("EaseExact with X", EaseExact, X, 0.1)
 test("EaseApprox with X", EaseApprox, X, 0.1)
+
+u = 5
+i = 5
+X_random = (numpy.random.randn(u, i) > 0.1) * 1.0
+test("EaseExact with X_random", EaseExact, X_random, 0.1)
+test("EaseApprox with X_random", EaseApprox, X_random, 0.1)
+
+u = 2000
+i = 1000
+X_large = (numpy.random.randn(u, i) > 1.0) * 1.0
+test("EaseExact with X_large", EaseExact, X_large, 0.1, small=False)
+test(
+    "EaseApprox with X_large", EaseApprox, X_large, 0.1, small=False
+)  # not good performanct
